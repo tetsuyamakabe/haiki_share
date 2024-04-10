a60372039c89e3110095001aec1af5fea8a76b3c
tetsuyamakabe <tetsuya2490@icloud.com>
2024-04-11 02:05:40 +0900

    コンビニ側の商品編集機能実装、なぜ更新せず新規作成されるのか

--- app/Http/Controllers/Products/Convenience/ProductController.php
+++ app/Http/Controllers/Products/Convenience/ProductController.php
@@ -9,6 +9,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\ProductPicture;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Convenience\ProductEditRequest;
 use App\Http\Requests\Convenience\ProductSaleRequest;
 
 class ProductController extends Controller
@@ -16,7 +17,9 @@ class ProductController extends Controller
     // 商品一覧画面の表示
     public function showProductIndex()
     {
-        return view('products.convenience.productIndex');
+        // 商品一覧を取得し、関連付けられた画像もロードする
+        $products = Product::with('pictures')->get();
+        return view('products.convenience.productIndex', ['products' => $products]);
     }
 
     // 商品出品一覧画面の表示
@@ -24,16 +27,17 @@ class ProductController extends Controller
     {
         $user = User::findOrFail($userId);
         $categories = Category::all();
-        return view('products.convenience.productSale', compact('user', 'categories'));
+        return view('products.convenience.productSale', ['user' => $userId, 'categories' => $categories]);
     }
-
 
     // 商品出品処理（商品の投稿）
     public function createProductSale(ProductSaleRequest $request, $userId)
     {
+        \Log::info('createProductSaleメソッドが実行されます。');
         \Log::info('$userIdは、', [$userId]);
         \Log::info('リクエストは、:', $request->all());
 
+        // 出品する商品情報の登録
         $product = new Product();
         $product->name = $request->name;
         $product->price = $request->price;
@@ -45,20 +49,61 @@ class ProductController extends Controller
 
         // 商品画像の処理
         if ($request->hasFile('product_picture')) {
-            $productPicture = $request->file('product_picture');
-            $extension = $productPicture->getClientOriginalExtension();
-            $fileName = sha1($productPicture->getClientOriginalName()) . '.' . $extension;
-            $productPicturePath = $productPicture->storeAs('public/product_pictures', $fileName);
+            $picture = $request->file('product_picture');
+            $extension = $picture->getClientOriginalExtension();
+            $fileName = sha1($picture->getClientOriginalName()) . '.' . $extension;
+            $picturePath = $picture->storeAs('public/product_pictures', $fileName);
 
             // 商品画像をproduct_picturesテーブルに保存
-            $productPictureModel = new ProductPicture();
-            $productPictureModel->product_id = $product->id;
-            $productPictureModel->file = $fileName;
-            $productPictureModel->save();
+            $productPicture = new ProductPicture();
+            $productPicture->product_id = $product->id;
+            $productPicture->file = $fileName;
+            $productPicture->save();
         }
 
         return redirect()->route('convenience.mypage.show', ['user' => $userId])->with('flash_message', '商品出品が完了しました');
     }
 
+    // 商品編集画面の表示
+    public function showProductEdit(Request $request, $productId)
+    {
+        \Log::info('showProductEditメソッドが実行されます。');
+        \Log::info('$productIdは、', [$productId]);
+        // dd($productId);
+        $product = Product::findOrFail($productId);
+        // dd($product);
+        $categories = Category::all();
+        return view('products.convenience.productEdit', ['product' => $product, 'categories' => $categories]);
+    }
 
+    // 商品編集・更新処理
+    public function editProduct(ProductEditRequest $request, $productId)
+    {
+        \Log::info('editProductメソッドが実行されます。');
+        \Log::info('$productIdは、', [$productId]);
+        \Log::info('リクエストは、:', $request->all());
+
+        // 出品する商品の更新
+        $product = Product::findOrFail($productId);
+        $product->name = $request->input('name');
+        $product->price = $request->input('price');
+        $product->category_id = $request->input('category');
+        $product->expiration_date = Carbon::parse($request->input('expiration_date'));
+        $product->convenience_store_id = $product->convenience_store_id;
+
+        $product->save();
+
+        // 商品画像の処理
+        if ($request->hasFile('product_picture')) {
+            $picture = $request->file('product_picture');
+            $extension = $picture->getClientOriginalExtension();
+            $fileName = sha1($picture->getClientOriginalName()) . '.' . $extension;
+            $picturePath = $picture->storeAs('public/product_pictures', $fileName);
+
+            $productPicture->product_id = $product->id;
+            $productPicture->file = $fileName;
+            $productPicture->save();
+        }
+        return redirect()->route('convenience.productIndex.show')->with('flash_message', '商品の編集が完了しました');
+    }
 }
