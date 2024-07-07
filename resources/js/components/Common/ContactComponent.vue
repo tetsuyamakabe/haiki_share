2dbe3c84a8a20af75be049dbf23979cfa59df106
tetsuyamakabe <tetsuya2490@icloud.com>
2024-07-07 11:42:51 +0900

    Vuelidateを使ってフロントエンド側のバリデーション実装（TOP画面のお問い合わせフォーム）

--- resources/js/components/Common/ContactComponent.vue
+++ resources/js/components/Common/ContactComponent.vue
@@ -72,7 +72,6 @@ export default {
                 required,
                 maxLength: maxLength(255),
             },
-
         },
     },
 
--- resources/js/components/Common/TopComponent.vue
+++ resources/js/components/Common/TopComponent.vue
@@ -119,15 +119,24 @@
                     <!-- お名前 -->
                     <label for="name" class="c-label">お名前<span class="c-badge">必須</span></label>
                     <span v-if="errors && errors.name" class="c-error">{{ errors.name[0] }}</span>
-                    <input v-model="formData.name" id="name" type="name" class="c-input" :class="{ 'is-invalid': errors && errors.name }" autocomplete="name">
+                    <span v-if="$v.formData.name.$error && $v.formData.name.$dirty" class="c-error">お名前が入力されていません。</span>
+                    <span v-if="$v.formData.name.$error && !$v.formData.name.maxLength && $v.formData.name.$dirty" class="c-error">お名前は、255文字以内で入力してください。</span>
+                    <input v-model="formData.name" id="name" type="text" class="c-input" @blur="$v.formData.name.$touch()" :class="{'is-invalid': $v.formData.name.$error && $v.formData.name.$dirty, 'is-valid': !$v.formData.name.$error && $v.formData.name.$dirty}" autocomplete="name">
+
                     <!-- メールアドレス -->
                     <label for="email" class="c-label">メールアドレス<span class="c-badge">必須</span></label>
                     <span v-if="errors && errors.email" class="c-error">{{ errors.email[0] }}</span>
-                    <input v-model="formData.email" id="email" type="email" class="c-input" :class="{ 'is-invalid': errors && errors.email }" autocomplete="email">
+                    <span v-if="$v.formData.email.$error && $v.formData.email.$dirty" class="c-error">メールアドレスが入力されていません。</span>
+                    <span v-if="$v.formData.email.$error && !$v.formData.email.maxLength && $v.formData.email.$dirty" class="c-error">メールアドレスは、255文字以内で入力してください。</span>
+                    <span v-if="$v.formData.email.$error && !$v.formData.email.email && $v.formData.email.$dirty" class="c-error">有効なメールアドレスを入力してください。</span>
+                    <input v-model="formData.email" id="email" type="text" class="c-input" @blur="$v.formData.email.$touch()" :class="{'is-invalid': $v.formData.email.$error && $v.formData.email.$dirty, 'is-valid': !$v.formData.email.$error && $v.formData.email.$dirty}" autocomplete="email">
+
                     <!--お問い合わせ内容 -->
                     <label for="contact" class="c-label">お問い合わせ内容<span class="c-badge">必須</span></label>
                     <span v-if="errors && errors.contact" class="c-error">{{ errors.contact[0] }}</span>
-                    <textarea v-model="formData.contact" maxlength="255" id="contact" type="text" class="c-textarea" cols="30" rows="10" @keyup="countCharacters" :class="{ 'is-invalid': errors && errors.contact }"></textarea>
+                    <span v-if="$v.formData.contact.$error && $v.formData.contact.$dirty" class="c-error">お問い合わせ内容が入力されていません。</span>
+                    <span v-if="$v.formData.contact.$error && !$v.formData.contact.maxLength && $v.formData.contact.$dirty" class="c-error">お問い合わせ内容は、255文字以内で入力してください。</span>
+                    <textarea v-model="formData.contact" maxlength="255" id="contact" type="text" class="c-textarea" cols="30" rows="10" @keyup="countCharacters" @blur="$v.formData.contact.$touch()" :class="{'is-invalid': $v.formData.contact.$error && $v.formData.contact.$dirty, 'is-valid': !$v.formData.contact.$error && $v.formData.contact.$dirty}"></textarea>
                     <span class="c-textarea--count">{{ formData.contact.length }} / 255文字</span>
                     <!-- 送信ボタン -->
                     <button type="submit" class="c-button c-button--submit c-button--main">送信する</button>
@@ -139,6 +148,7 @@
 
 <script>
 import Toast from '../Parts/Toast.vue'; // Toastコンポーネントをインポート
+import { required, maxLength, email } from 'vuelidate/lib/validators'; // Vuelidateからバリデータをインポート
 
 export default {
     components: {
@@ -155,6 +165,24 @@ export default {
             textareaCount: 0, // お問い合わせ内容の文字数カウント初期値
             errors: null, // エラーメッセージ
         };
+    },
+
+    validations: { // フロント側のバリデーション
+        formData: {
+            name: {
+                required,
+                maxLength: maxLength(255),
+            },
+            email: {
+                required,
+                email,
+                maxLength: maxLength(255),
+            },
+            contact: {
+                required,
+                maxLength: maxLength(255),
+            },
+        },
     },
 
     methods: {
